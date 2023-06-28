6b2055af9494479e2b92a26925cc42f5bdd947f5
Selton A. De Melo <selton.alvess1@gmail.com>
2023-06-28 01:32:02 -0300

    implementação do navigation entre fragment finalizada.

--- app/src/main/java/com/mycompany/movies/view/HomeActivity.kt
+++ app/src/main/java/com/mycompany/movies/view/HomeActivity.kt
@@ -1,15 +1,27 @@
 package com.mycompany.movies.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.mycompany.movies.R
 import com.mycompany.movies.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navigationBottom,navController)
     }
 }
